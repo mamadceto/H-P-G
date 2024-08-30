614b515c38907a63feea1c0ab9eecb0dbabf00ad
CeTo <103120386+mamadceto@users.noreply.github.com>
2024-08-30 12:40:04 +0330

    Update script.js

--- script.js
+++ script.js
@@ -69,6 +69,20 @@ document.addEventListener('DOMContentLoaded', () => {
             promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
             timing: 20000,
             attempts: 20,
+        },
+        11: {
+            name: 'Zoopolis',
+            appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+            promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+            timing: 20000,
+            attempts: 20,
+        },
+        12: {
+            name: 'Gangs Wars',
+            appToken: 'b6de60a0-e030-48bb-a551-548372493523',
+            promoId: 'c7821fa7-6632-482c-9635-2bd5798585f9',
+            timing: 20000,
+            attempts: 20,
         }
     };
 
